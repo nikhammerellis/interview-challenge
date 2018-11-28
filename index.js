d0ba92302245837a66fa18be3257c8006d1cb2ec
Nik <nikhammerellis@gmail.com>
2018-11-28 13:41:26 -0800

    added some mongo set up and folders

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const keys = require("./config/keys");
+//require("./models/Color");
+
+//connect to DB
+mongoose.connect(keys.mongoURI);
+//initialize app
 const app = express();
+
+app.use(bodyParser.json());
+
+//require("./routes/colorRoutes")(app);
 
 app.get("/", (req, res) => {
   res.send({ hi: "there" });
