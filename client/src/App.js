e2e8c1ecb5a0eab91a27ec59edfef50cb0daa087
Nik <nikhammerellis@gmail.com>
2018-11-28 16:32:20 -0800

    test front end fix in production

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,57 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import logo from "./logo.svg";
 import "./App.css";
+
+const Landing = () => {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Hi there
+      </a>
+    </header>
+  );
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Hi there
-          </a>
-        </header>
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Switch>
+              <Route exact path="/" component={Landing} />
+            </Switch>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
 }
 
 export default App;
+//
+// <header className="App-header">
+//   <img src={logo} className="App-logo" alt="logo" />
+//   <p>
+//     Edit <code>src/App.js</code> and save to reload.
+//   </p>
+//   <a
+//     className="App-link"
+//     href="https://reactjs.org"
+//     target="_blank"
+//     rel="noopener noreferrer"
+//   >
+//     Hi there
+//   </a>
+// </header>
